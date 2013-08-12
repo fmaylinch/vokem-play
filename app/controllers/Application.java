51959421fbf193163d0cc1e4e5b75ca9a0c7f9b4
Ferran Maylinch <ferranmaylinch@gmail.com>
2013-08-12 15:45:59 +0200

    Test page (at  (path /test) that uses twitter bootstrap, font awesome and includes more head elements (just a test.css).

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,9 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+
+    public static Result test() {
+        return ok(test.render());
+    }
+
 }
